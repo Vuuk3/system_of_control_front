f61ea5bb2262c8399aebc7c537b6fe235cb1b554
Vuuk3 <daniil@archlinux.localdomain>
2026-03-19 10:22:55 +0300

    Added adaptivity to register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -65,94 +65,98 @@ function Register() {
       <form
         noValidate
         method="post"
-        className={styles["login"]}
+        className={styles["register"]}
         onSubmit={(e) => {
           e.preventDefault();
           submit();
         }}
       >
-        <h2 className={styles["header-main"]}>Staff Tracker</h2>
-        <h2 className={styles["header-login"]}>Регистрация</h2>
-        <div className={styles["company-info"]}>
-          <h3 className={styles["register-h3"]}>Данные о компании</h3>
-          {REGISTER_FIELDS.slice(0, REGISTER_FIELDS.length - 3).map((field) => (
-            <FormField
-              key={field.name}
-              name={field.name}
-              inputType={field.inputType}
-              maxLength={field.maxLength}
-              placeholder={field.placeholder}
-              logo={field.logo}
-              value={values[field.name]}
-              error={errors[field.name]}
+        <div className={styles["register-scroll"]}>
+          <h2 className={styles["header-main"]}>Staff Tracker</h2>
+          <h2 className={styles["header-login"]}>Регистрация</h2>
+          <div className={styles["company-info"]}>
+            <h3 className={styles["register-h3"]}>Данные о компании</h3>
+            {REGISTER_FIELDS.slice(0, REGISTER_FIELDS.length - 3).map(
+              (field) => (
+                <FormField
+                  key={field.name}
+                  name={field.name}
+                  inputType={field.inputType}
+                  maxLength={field.maxLength}
+                  placeholder={field.placeholder}
+                  logo={field.logo}
+                  value={values[field.name]}
+                  error={errors[field.name]}
+                  handleChange={handleChange}
+                />
+              ),
+            )}
+            <SelectField
+              placeholder="Организационно-правовая форма"
+              name="legalForm"
+              defaultValue={values.legal_form}
+              handleChange={handleChange}
+              options={[
+                { value: "ооо", text: "ООО" },
+                { value: "ип", text: "ИП" },
+                { value: "ао", text: "АО" },
+                { value: "пао", text: "ПАО" },
+                { value: "нао", text: "НАО" },
+                { value: "гуп", text: "ГУП" },
+                { value: "муп", text: "МУП" },
+                { value: "нко", text: "НКО" },
+              ]}
+            />
+          </div>
+          <div className={styles["contact-info"]}>
+            <h3 className={styles["register-h3"]}>Данные контактного лица</h3>
+            {REGISTER_FIELDS.slice(
+              REGISTER_FIELDS.length - 3,
+              REGISTER_FIELDS.length - 1,
+            ).map((field) => (
+              <FormField
+                key={field.name}
+                name={field.name}
+                inputType={field.inputType}
+                maxLength={field.maxLength}
+                placeholder={field.placeholder}
+                logo={field.logo}
+                value={values[field.name]}
+                error={errors[field.name]}
+                handleChange={handleChange}
+              />
+            ))}
+          </div>
+          <div className={styles["manager-info"]}>
+            <h3 className={styles["register-h3"]}>Данные пользователя</h3>
+            {REGISTER_FIELDS.slice(-1).map((field) => (
+              <FormField
+                key={field.name}
+                name={field.name}
+                inputType={field.inputType}
+                maxLength={field.maxLength}
+                placeholder={field.placeholder}
+                logo={field.logo}
+                value={values[field.name]}
+                error={errors[field.name]}
+                handleChange={handleChange}
+              />
+            ))}
+            <PasswordField
+              maxLength={20}
+              placeholder="Пароль"
+              value={values["password"]}
+              error={errors["password"]}
               handleChange={handleChange}
             />
-          ))}
-          <SelectField
-            placeholder="Организационно-правовая форма"
-            name="legalForm"
-            defaultValue={values.legal_form}
-            handleChange={handleChange}
-            options={[
-              { value: "ооо", text: "ООО" },
-              { value: "ип", text: "ИП" },
-              { value: "ао", text: "АО" },
-              { value: "пао", text: "ПАО" },
-              { value: "нао", text: "НАО" },
-              { value: "гуп", text: "ГУП" },
-              { value: "муп", text: "МУП" },
-              { value: "нко", text: "НКО" },
-            ]}
+          </div>
+          <SubmitButton
+            className={styles["button"]}
+            text="Регистрация"
+            disabled={Object.values(errors).some(Boolean)}
+            handleClick={() => setErrors(validation(values))}
           />
         </div>
-        <div className={styles["contact-info"]}>
-          <h3 className={styles["register-h3"]}>Данные контактного лица</h3>
-          {REGISTER_FIELDS.slice(
-            REGISTER_FIELDS.length - 3,
-            REGISTER_FIELDS.length - 1,
-          ).map((field) => (
-            <FormField
-              key={field.name}
-              name={field.name}
-              inputType={field.inputType}
-              maxLength={field.maxLength}
-              placeholder={field.placeholder}
-              logo={field.logo}
-              value={values[field.name]}
-              error={errors[field.name]}
-              handleChange={handleChange}
-            />
-          ))}
-        </div>
-        <div className={styles["manager-info"]}>
-          <h3 className={styles["register-h3"]}>Данные пользователя</h3>
-          {REGISTER_FIELDS.slice(-1).map((field) => (
-            <FormField
-              key={field.name}
-              name={field.name}
-              inputType={field.inputType}
-              maxLength={field.maxLength}
-              placeholder={field.placeholder}
-              logo={field.logo}
-              value={values[field.name]}
-              error={errors[field.name]}
-              handleChange={handleChange}
-            />
-          ))}
-          <PasswordField
-            maxLength={20}
-            placeholder="Пароль"
-            value={values["password"]}
-            error={errors["password"]}
-            handleChange={handleChange}
-          />
-        </div>
-        <SubmitButton
-          className={styles["button"]}
-          text="Регистрация"
-          disabled={Object.values(errors).some(Boolean)}
-          handleClick={() => setErrors(validation(values))}
-        />
       </form>
     </div>
   );
